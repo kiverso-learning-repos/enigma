e508c5125883f95bb829c8c7c00080ac156742eb
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-20 18:44:40 -0600

    refactor encrypt and decrypt methods to use cipher/plain text objects

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,31 +1,8 @@
 class Enigma
-attr_reader :characters
-
-  def initialize(characters = ("a".."z").to_a << " ")
-    @characters = characters
-  end
-
-  def get_encoded_character(character, shift)
-    if @characters.include?(character)
-      encoded_characters = @characters.rotate(shift)
-      encoded_characters[@characters.index(character)]
-    else
-      character
-    end
-  end
-
-  def shift_message(message, shifts, encryption = [])
-    message.downcase.each_char do |character|
-      encryption << get_encoded_character(character, shifts.first)
-      shifts = shifts.rotate(1)
-    end
-    encryption.join
-  end
 
   def encrypt(message, key = generate_key, date = current_date)
     shifts = Shift.new(key, date).total_shifts.values
-    encryption = {}
-    encryption[:encryption] = shift_message(message, shifts)
+    encryption = PlainText.new(message).encrypt(shifts)
     encryption[:key] = key
     encryption[:date] = date
     encryption
@@ -33,9 +10,7 @@ attr_reader :characters
 
   def decrypt(message, key, date = current_date)
     shifts = Shift.new(key, date).total_shifts.values
-    unshifts = shifts.map{|shift_value| shift_value * -1}
-    decryption = {}
-    decryption[:decryption] = shift_message(message, unshifts)
+    decryption = CipherText.new(message).decrypt(shifts)
     decryption[:key] = key
     decryption[:date] = date
     decryption
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,23 +9,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  # def test_it_has_readable_attributes
-  #   expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
-  #               "j", "k", "l", "m", "n", "o", "p", "q", "r",
-  #               "s", "t", "u", "v", "w", "x", "y", "z", " "]
-  #   assert_equal expected, @enigma.characters
-  # end
-  #
-  # def test_it_can_get_encoded_character
-  #   assert_equal "k", @enigma.get_encoded_character("h", 3)
-  #   assert_equal "@", @enigma.get_encoded_character("@", 33)
-  # end
-  #
-  # def test_it_can_shift_message
-  #   assert_equal "keder ohulw", @enigma.shift_message("hello world", [3, 27, 73, 20])
-  #   assert_equal "keder ohulw.", @enigma.shift_message("Hello World.", [3, 27, 73, 20])
-  # end
-
   def test_it_can_encrypt_with_given_date_and_key
     expected = {
                 encryption: "keder ohulw",
@@ -94,45 +77,45 @@ class EnigmaTest < Minitest::Test
     assert_equal "00006", @enigma.generate_key
   end
 
-  def test_it_can_read_from_txt_file
-    filepaths = ["./text_files/test_message.txt", "./text_files/test_message_encrypted.txt"]
-    @enigma.stubs(:user_input).returns(filepaths)
-    expected = "Hello World!"
-    assert_equal expected, @enigma.read_from_txt
-    assert_equal expected, @enigma.read_from_txt("./text_files/test_message.txt")
-  end
-
-  def test_it_can_write_to_txt
-    filepaths = ["./text_files/test_message.txt", "./text_files/test_message_write.txt"]
-    @enigma.stubs(:user_input).returns(filepaths)
-    assert_equal false, File.exists?("./text_files/test_message_write.txt")
-    @enigma.write_to_txt("hello world")
-    assert_equal true, File.exists?("./text_files/test_message_write.txt")
-    expected = "hello world"
-    assert_equal expected, @enigma.read_from_txt("./text_files/test_message_write.txt")
-    File.delete("./text_files/test_message_write.txt")
-  end
-
-  def test_it_can_encrypt_from_file
-    filepaths = ["./text_files/test_message.txt", "./text_files/encrypted.txt"]
-    @enigma.stubs(:user_input).returns(filepaths)
-    Time.stubs(:now).returns(Time.new(1995, 04, 8))
-    @enigma.stubs(:generate_number).returns(2715)
-    assert_equal false, File.exists?("./text_files/encrypted.txt")
-    expected = "Created './text_files/encrypted.txt' with the key 02715 and date 040895"
-    assert_equal expected, @enigma.encrypt_from_file
-    assert_equal "keder ohulw!", @enigma.read_from_txt("./text_files/encrypted.txt")
-    File.delete("./text_files/encrypted.txt")
-  end
-
-  def test_it_can_decrypt_from_file
-    input = ["./text_files/encrypted_test.txt", "./text_files/decrypted.txt","02715", "040895"]
-    @enigma.stubs(:user_input).returns(input)
-    Time.stubs(:now).returns(Time.new(1995, 04, 8))
-     assert_equal false, File.exists?("./text_files/decrypted.txt")
-    expected = "Created './text_files/decrypted.txt' with the key 02715 and date 040895"
-    assert_equal expected, @enigma.decrypt_from_file
-    assert_equal "hello world!", @enigma.read_from_txt("./text_files/decrypted.txt")
-    File.delete("./text_files/decrypted.txt")
-  end
+  # def test_it_can_read_from_txt_file
+  #   filepaths = ["./text_files/test_message.txt", "./text_files/test_message_encrypted.txt"]
+  #   @enigma.stubs(:user_input).returns(filepaths)
+  #   expected = "Hello World!"
+  #   assert_equal expected, @enigma.read_from_txt
+  #   assert_equal expected, @enigma.read_from_txt("./text_files/test_message.txt")
+  # end
+  #
+  # def test_it_can_write_to_txt
+  #   filepaths = ["./text_files/test_message.txt", "./text_files/test_message_write.txt"]
+  #   @enigma.stubs(:user_input).returns(filepaths)
+  #   assert_equal false, File.exists?("./text_files/test_message_write.txt")
+  #   @enigma.write_to_txt("hello world")
+  #   assert_equal true, File.exists?("./text_files/test_message_write.txt")
+  #   expected = "hello world"
+  #   assert_equal expected, @enigma.read_from_txt("./text_files/test_message_write.txt")
+  #   File.delete("./text_files/test_message_write.txt")
+  # end
+  #
+  # def test_it_can_encrypt_from_file
+  #   filepaths = ["./text_files/test_message.txt", "./text_files/encrypted.txt"]
+  #   @enigma.stubs(:user_input).returns(filepaths)
+  #   Time.stubs(:now).returns(Time.new(1995, 04, 8))
+  #   @enigma.stubs(:generate_number).returns(2715)
+  #   assert_equal false, File.exists?("./text_files/encrypted.txt")
+  #   expected = "Created './text_files/encrypted.txt' with the key 02715 and date 040895"
+  #   assert_equal expected, @enigma.encrypt_from_file
+  #   assert_equal "keder ohulw!", @enigma.read_from_txt("./text_files/encrypted.txt")
+  #   File.delete("./text_files/encrypted.txt")
+  # end
+  #
+  # def test_it_can_decrypt_from_file
+  #   input = ["./text_files/encrypted_test.txt", "./text_files/decrypted.txt","02715", "040895"]
+  #   @enigma.stubs(:user_input).returns(input)
+  #   Time.stubs(:now).returns(Time.new(1995, 04, 8))
+  #    assert_equal false, File.exists?("./text_files/decrypted.txt")
+  #   expected = "Created './text_files/decrypted.txt' with the key 02715 and date 040895"
+  #   assert_equal expected, @enigma.decrypt_from_file
+  #   assert_equal "hello world!", @enigma.read_from_txt("./text_files/decrypted.txt")
+  #   File.delete("./text_files/decrypted.txt")
+  # end
 end
